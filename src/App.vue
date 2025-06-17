c72edc1def28c692d03dd60bb7e686dba9988123
Sebastian <sebastianlakla@gmail.com>
2025-06-17 19:16:48 +0200

    Add font size and weight for project

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,13 @@
 <style lang="scss">
 @import './assets/sass/reset.scss';
 @import './assets/sass/colors.scss';
+@import "./assets/sass/fonts.scss";
 div {
     height: 200px;
     background: $primary-instagram;
   p {
     color: $toggle-dark-theme;
-    
+    font-size: $primary-font-size;
   }
 }
 </style>
