aca328c444ab98d282f68637e2646e1994fc136a
Sebastian <sebastianlakla@gmail.com>
2025-06-17 19:20:37 +0200

    Delete test div and p tag for check do it all setup files works

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,8 @@
-<script setup></script>
+<script setup>
+</script>
 
 <template>
-  <div>
-    <p>heello</p>
-    </div>
 </template>
 
 <style lang="scss">
-@import './assets/sass/reset.scss';
-@import './assets/sass/colors.scss';
-@import "./assets/sass/fonts.scss";
-div {
-    height: 200px;
-    background: $primary-instagram;
-  p {
-    color: $toggle-dark-theme;
-    font-size: $primary-font-size;
-  }
-}
 </style>
