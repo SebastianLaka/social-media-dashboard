ab8de6de1ba350956d23a3236f7ee8b8e81d94cb
Sebastian <sebastianlakla@gmail.com>
2025-06-17 19:13:43 +0200

    Set colors for project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-<p>heello</p>
+  <div>
+    <p>heello</p>
+    </div>
 </template>
 
 <style lang="scss">
 @import './assets/sass/reset.scss';
+@import './assets/sass/colors.scss';
+div {
+    height: 200px;
+    background: $primary-instagram;
+  p {
+    color: $toggle-dark-theme;
+    
+  }
+}
 </style>
