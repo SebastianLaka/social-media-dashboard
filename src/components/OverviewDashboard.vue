7f4e2aad340f16a4879a22f39453b4281c984cca
Sebastian <sebastianlakla@gmail.com>
2025-06-26 19:58:05 +0200

    Add props for Overview dashboard component

--- src/components/OverviewDashboard.vue
+++ src/components/OverviewDashboard.vue
@@ -1,4 +1,22 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+  title: String,
+  src: {
+    type: String,
+    required: true,
+  },
+  followersCount: Number,
+  growthIcon: {
+    type: String,
+    required: false,
+  },
+  growthAmount: Number,
+  color: {
+    type: String,
+    default: 'transparent',
+  },
+})
+</script>
 <template>
   <section class="overview-dashboard">
     <h1 class="overview-dashboard__header">Overview - Today</h1>
@@ -7,12 +25,12 @@
 <style lang="scss" scoped>
 @import '../assets/sass/colors.scss';
 @import '../assets/sass/fonts.scss';
-.overview-dashboard{
-   padding: 0 0.25em; 
-   &__header{
+.overview-dashboard {
+  padding: 0 0.25em;
+  &__header {
     font-weight: changeFontWeight($bold-font-weight);
     color: changeFontColor($dark-grayish-blue);
     font-size: changeFontSize(1.75rem);
-   }
+  }
 }
 </style>
